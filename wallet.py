0fad6a1a9d08bd5350b14e1121da2b95a7eeac4a
Rutger Bevaart <rutger@bevaart.net>
2019-11-05 14:07:24 +0100

    missed

--- wallet.py
+++ wallet.py
@@ -8,12 +8,3 @@ class Wallet:
         self.tegoed = start_tegoed
     def betalen(self, bedrag):
         self.tegoed -= bedrag
-
-# instantiate the Parrot class
-player1 = Wallet(50)
-
-
-# access the class attributes
-print("player1 has a balance of {}" . format(player1.tegoed))
-player1.betalen(100)
-print("player1 heeft 10 betaald en nu een balance of {}" . format(player1.tegoed))
